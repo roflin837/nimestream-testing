a842dbfa3369264991d36c0c47e18155b45580f6
Wok Flinn Mau Femes <roflin2008@gmail.com>
2026-03-10 05:51:00 +0700

    Decrease fetcher timeout to 15 seconds
    Reduced timeout from 30 seconds to 15 seconds for faster response.

--- src/services/animeService.js
+++ src/services/animeService.js
@@ -5,7 +5,7 @@ const { BASE_URL } = require("../config/config");
 
 const fetcher = axios.create({
   baseURL: BASE_URL,
-  timeout: 30000,
+  timeout: 15000, // Turunin ke 15 detik biar gak nunggu kelamaan
   httpsAgent: new https.Agent({ rejectUnauthorized: false }),
   headers: {
     "User-Agent":
@@ -13,10 +13,8 @@ const fetcher = axios.create({
     Accept:
       "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
     "Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
-    Referer: "https://otakudesu.cloud/",
-    Host: "otakudesu.cloud", // Paksa host-nya sesuai target
+    Referer: BASE_URL, // Pake variabel BASE_URL aja biar dinamis
     Connection: "keep-alive",
-    "Upgrade-Insecure-Requests": "1",
   },
 });
 
